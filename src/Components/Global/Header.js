59ea8f7b84db45f17deb216012b35c18e72a9c5d
Aman-Singh-5 <amansingh.iotainfotech@gmail.com>
2023-07-10 16:51:14 +0530

    second commit

--- src/Components/Global/Header.js
+++ src/Components/Global/Header.js
@@ -2,22 +2,11 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Row from 'react-bootstrap/esm/Row';
-import Col from 'react-bootstrap/esm/Col';
+
 
 export const Header = () => {
     return(
         <>
-    <Container fluid>
-      <Row>
-        <Col lg={12}>
-          <div className='signin-div d-flex'>
-              <img src='images/Sign in.png' alt='pic'/>
-              <img src='images/Sign in.png' alt='pic'/>
-          </div>
-        </Col>
-      </Row>
-    </Container>
     <Navbar expand="lg" className="bg-body-transparent">
       <Container>
         <Navbar.Brand href="#"><img src='images/logo banner.png' alt='logo'/></Navbar.Brand>
@@ -25,7 +14,7 @@ export const Header = () => {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: '500px' }}
             navbarScroll
           >
             <Nav.Link href="#action1">Home</Nav.Link>
